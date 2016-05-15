a13a97320d0518d18f498780588f56bd7b381203
Ricardo Castro Tapia <ricardocastro0208@hotmail.com>
2016-05-15 17:16:29 -0500

    Initial commit

---
+++ app/src/androidTest/java/miprimerapp/castro/ricardo/com/miprimerappandroid/ApplicationTest.java
@@ -0,0 +1,13 @@
+package miprimerapp.castro.ricardo.com.miprimerappandroid;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
